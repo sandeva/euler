bf5c388c7dc090bb96d07dfdce0d815a5a04d562
Sandeva Goonetilleke <sandeva@Sandevas-MacBook-Air.local>
2024-05-22 12:36:37 +0530

    5.py

---
+++ 5.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+N = 20
+def primes(lim):
+    p = []
+    for i in range(2, lim + 1):
+        for j in p:
+            if not i % j:
+                break
+        else:
+            p.append(i)
+            yield i
+
+def pow(p, N):
+    r = p
+    while r <= N:
+        r *= p
+    return r // p
+
+ps = list(primes(N))
+print(ps)
+ps = [pow(x, N) for x in ps]
+print(ps)
+print(reduce(int.__mul__, ps, 1))
